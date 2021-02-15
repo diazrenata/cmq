08f3255a79b9ae406dc69f946b0347b9d5e5bba8
Renata Diaz <diaz.renatam@gmail.com>
2021-02-15 12:54:14 -0500

    Update pipeline.R

--- analysis/pipeline.R
+++ analysis/pipeline.R
@@ -33,7 +33,7 @@ if(grepl("ufhpc", nodename)) {
        verbose = 2,
        parallelism = "clustermq",
        jobs = 50,
-       caching = "master", memory_strategy = "autoclean") # Important for DBI caches!
+       caching = "main", memory_strategy = "autoclean") # Important for DBI caches!
 } else {
   library(clustermq)
   options(clustermq.scheduler = "multicore")
